430f3b0a968a6d7809e11105d0569d9e41f6352c
carolinewolf <27232948+carolinewolf@users.noreply.github.com>
2018-10-14 21:41:36 +0200

    Anleitung
    - added toolbar
    - added menu-button to toolbar
    - added buttonshape layout xml

--- HAWOERallye/app/src/main/java/de/caroliwo/hawoe_rallye/Introduction.java
+++ HAWOERallye/app/src/main/java/de/caroliwo/hawoe_rallye/Introduction.java
@@ -1,13 +1,40 @@
 package de.caroliwo.hawoe_rallye;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Introduction extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
+
+        //Menü + Titel der App in Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_icon);
+
+    }
+
+    //actionListener für "Hamburger" im main_menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //hier Intent mit Verweis aufs Menü einfügen
+        //ODER
+        //navigation drawer
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
