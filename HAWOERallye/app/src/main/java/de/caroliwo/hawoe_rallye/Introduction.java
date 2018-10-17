65c7bdd19e9997c47bc13449f070545545558bb2
carolinewolf <27232948+carolinewolf@users.noreply.github.com>
2018-10-17 20:44:42 +0200

    added Menu

--- HAWOERallye/app/src/main/java/de/caroliwo/hawoe_rallye/Introduction.java
+++ HAWOERallye/app/src/main/java/de/caroliwo/hawoe_rallye/Introduction.java
@@ -1,40 +1,47 @@
 package de.caroliwo.hawoe_rallye;
 
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class Introduction extends AppCompatActivity {
-
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_introduction);
+        setContentView(R.layout.activity_menu);
 
-        //Menü + Titel der App in Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Menü einblenden
+        drawer=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //Menübutton + Titel der App in Toolbar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_icon);
-
     }
 
-    //actionListener für "Hamburger" im main_menu
+    //Reaktion bei Touch auf Zurück-Button
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //hier Intent mit Verweis aufs Menü einfügen
-        //ODER
-        //navigation drawer
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
-        return true;
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
     }
+
 }
