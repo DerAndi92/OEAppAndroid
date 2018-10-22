f78c1f4d51f8c2a987c97debef5c763e8baeb0e3
carolinewolf <27232948+carolinewolf@users.noreply.github.com>
2018-10-22 13:46:18 +0200

    GroupActitivity für Gruppenwahl

--- HAWOERallye/app/src/main/java/de/caroliwo/hawoe_rallye/GroupActivity.java
+++ HAWOERallye/app/src/main/java/de/caroliwo/hawoe_rallye/GroupActivity.java
@@ -1,24 +1,24 @@
 package de.caroliwo.hawoe_rallye;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LogInActivity extends AppCompatActivity {
+public class GroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_group);
 
-        Button loginButton = findViewById(R.id.logInBTN);
+        Button joinButton = findViewById(R.id.joinBTN);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LogInActivity.this, GroupActivity.class));
+                startActivity(new Intent(GroupActivity.this, MainActivity.class));
             }
         });
     }
